387c489837524729779197af3a57d00f1ad00bf1
weichen11011 <99935264+weichen11011@users.noreply.github.com>
2023-08-15 00:01:15 +0800

    Create climbStairs.cpp

---
+++ leetcode/climbStairs.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+# include <vector>
+# include <string>
+# include <cctype>
+using namespace std;
+class Solution {
+public:
+    int climbStairs(int n) {
+        long long dp[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i=2;i<n+1;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+int main(){
+    Solution c;
+    int ans = c.climbStairs(6);
+    cout<<ans<<endl;
+}
